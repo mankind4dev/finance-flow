88463c02d2a66f7f43bac436e01b1b5dedba5715
mankind4dev <ogunmakin120@gmail.com>
2024-11-29 23:03:03 +0100

    drevenAi section implement successful

---
+++ src/app/components/landingPage/DrivenAi.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import React from "react";
+
+const DrivenAi = () => {
+  return (
+    <>
+      <div className="flex flex-col bg-[rgba(255,255,255,1)]">
+        <div className="flex w-full flex-wrap gap-8 justify-center text-center mt-24 py-16">
+          <Image
+            src="/images/ai1.png"
+            width={500}
+            height={500}
+            alt="tracking-image"
+            objectFit="contain"
+          />
+          <Image
+            src="/images/ai2.png"
+            width={500}
+            height={500}
+            alt="report-image"
+            objectFit="contain"
+          />
+          <Image
+            src="/images/ai3.png"
+            width={500}
+            height={500}
+            alt="report-image"
+            objectFit="contain"
+          />
+        </div>
+        <button className=" self-center p-[20px] px-[30px] text-center rounded-[26px] text-xl font-normal mb-6 text-[rgba(230,242,242,1)]  bg-[rgba(66,133,244,1)]">
+          Get Started
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default DrivenAi;
--- src/app/page.js
+++ src/app/page.js
@@ -2,27 +2,32 @@ import Image from "next/image";
 import Tracking from "./components/landingPage/Tracking";
 import SetUp from "./components/landingPage/SetUp";
 import Reports from "./components/landingPage/Reports";
-import LinkBank from "./components/landingPage/LinkBank"; 
+import LinkBank from "./components/landingPage/LinkBank";
 import Swipers from "./components/Swipers";
-import Test from "./components/Test"; 
+import Test from "./components/Test";
 import Swipper from "./components/landingPage/Swipper";
+import Frequent from "./components/landingPage/Frequent";
+import DrivenAi from "./components/landingPage/DrivenAi";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen min-w-full max-w-lg px-8 pb-2   "> 
+    <div className="flex flex-col min-h-screen min-w-full max-w-lg px-8    ">
       <section className="flex flex-col gap-y-16 ">
         <Tracking />
         <SetUp />
         <Reports />
-      </section> 
-      <section className="flex w-full my-20"> 
+      </section>
+      <section className="flex w-full my-20">
         <Swipper />
       </section>
 
       <section className="">
-        
+        <Frequent />
       </section>
-       
+
+      <section className="">
+        <DrivenAi />
+      </section>
     </div>
   );
 }
