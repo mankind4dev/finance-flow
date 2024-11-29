9a8c4637690df94afba3d212643ed20c31b4d38c
mankind4dev <ogunmakin120@gmail.com>
2024-11-29 14:03:14 +0100

    Header link

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -14,11 +14,7 @@ const Header = () => {
       <div className="bg-[rgba(66,133,244,1)]">
         <div className="flex text-center justify-between py-8 pr-16 pl-16">
           <Link href={"/"}>
-          <Image
-              src="/logo.png"
-              alt="logo"
-              width={200} height={100}
-            />
+            <Image src="/logo.png" alt="logo" width={200} height={100} />
           </Link>
           <ul className="flex text-center self-center">
             <li className="text-[20px] font-[500] mr-6 text-[rgba(0,0,0,1)] hover:text-gray-200">
@@ -35,13 +31,16 @@ const Header = () => {
             </li>
           </ul>
           <div className="flex justify-between items-center">
-            <li className="text-[20px] font-[500] mr-6 bg-[rgba(230,242,242,1)] text-[rgba(66,133,244,1)] list-none py-4 px-12 rounded-[50px]">
-              <Link href={"/signup"}>Get Started</Link>
-            </li>
-            <li className="text-[20px] font-[500] mr-6 bg-[rgba(230,242,242,1)] text-[rgba(66,133,244,1)] list-none py-4 px-12 rounded-[50px]">
-              <Link href={"/signin"}>Sign In</Link>
-            </li>
-
+            <Link href={"/signup"}>
+              <li className="text-[20px] font-[500] mr-6 bg-[rgba(230,242,242,1)] text-[rgba(66,133,244,1)] list-none py-4 px-12 rounded-[50px]">
+                Get Started
+              </li>
+            </Link>
+            <Link href={"/signin"}>
+              <li className="text-[20px] font-[500] mr-6 bg-[rgba(230,242,242,1)] text-[rgba(66,133,244,1)] list-none py-4 px-12 rounded-[50px]">
+                Sign In
+              </li>
+            </Link>
           </div>
         </div>
       </div>
